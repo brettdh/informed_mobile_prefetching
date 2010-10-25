eae33c65acead0770466a1ad1a2dbd12c11264b2
Brett Higgins <brettdh@eecs.umich.edu>
2010-10-25 16:10:31 -0400

    Trivial implementation of the library compiles.  Now for testing.

--- src/edu/umich/eac/EnergyAdaptiveCache.java
+++ src/edu/umich/eac/EnergyAdaptiveCache.java
@@ -1,12 +1,34 @@
 package edu.umich.eac;
 
-import java.util.ExecutorService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.ExecutionException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+import android.util.Log;
+
+import edu.umich.eac.FetchFuture;
 
 public class EnergyAdaptiveCache {
+    private String TAG = EnergyAdaptiveCache.class.getName();
+    
     private ExecutorService executor;
-    private Set<FetchFuture<?> > prefetches;
+    
+    // contains prefetches yet to be sent.
+    private BlockingQueue<FetchFuture<?> > prefetchQueue;
+    
+    // contains prefetches that have been sent, potentially having results.
+    // XXX: might be unnecessary.  Only the application needs references
+    //      to these objects
+    private Set<FetchFuture<?> > prefetchCache;
     
     /** Hints a future access and gets a Future for that access.
      *
@@ -18,32 +40,46 @@ public class EnergyAdaptiveCache {
      *  @return A Future that can be used later to actually carry out
      *          the hinted access, via get().
      */
-    public Future<T> prefetch(Callable<T> fetcher) {
-        Future<T> fetchFuture = new FetchFuture<T>(fetcher, this);
-        prefetches.add(fetchFuture);
+    public <V> Future<V> prefetch(Callable<V> fetcher) {
+        try {
+            FetchFuture<V> fetchFuture = new FetchFuture<V>(fetcher, this);
+            prefetchQueue.put(fetchFuture);
         
-        return fetchFuture;
+            return fetchFuture;
+        } catch (InterruptedException e) {
+            // the queue has no max size, so this won't happen.
+            assert false;
+            return null;
+        }
     }
     
-    Future<T> submit(FetchFuture<T> fetchFuture) {
+    <V> Future<V> submit(FetchFuture<V> fetchFuture) {
         // TODO: record whatever necessary information
         return executor.submit(fetchFuture.fetcher);
     }
     
-    void remove(FetchFuture<T> fetchFuture) {
+    void remove(FetchFuture<?> fetchFuture) {
         evaluatePrefetchDecision(fetchFuture);
-        prefetches.remove(fetchFuture);
+        prefetchCache.remove(fetchFuture);
     }
     
-    private void evaluatePrefetchDecision(FetchFuture<T> fetchFuture) {
+    private <V> void evaluatePrefetchDecision(FetchFuture<V> fetchFuture) {
         // TODO: estimate whether I "made a mistake" with this fetch
         //       e.g. too early, too late
     }
     
     public EnergyAdaptiveCache() {
-        // TODO: init members
-        // TODO: start prefetch thread
+        executor = Executors.newCachedThreadPool();
+        prefetchQueue = new LinkedBlockingQueue<FetchFuture<?> >();
+        prefetchCache = Collections.synchronizedSet(
+            new TreeSet<FetchFuture<?> >()
+        );
+        
+        prefetchThread = new PrefetchThread();
+        prefetchThread.start();
     }
+    
+    private PrefetchThread prefetchThread;
     
     private class PrefetchThread extends Thread {
         public void run() {
@@ -55,6 +91,25 @@ public class EnergyAdaptiveCache {
              *             perhaps registering an intnw notification callback
              *             to be woken up at that point
              */
+            
+            // To start, we'll just implement the most aggressive strategy:
+            //   1) Wait for a prefetch request to be enqueued
+            //   2) When it arrives, get() it immediately (but don't wait)
+            
+            while (true) {
+                try {
+                    FetchFuture<?> prefetch = prefetchQueue.take();
+                    prefetchCache.add(prefetch);
+
+                    prefetch.startAsync();
+                } catch (InterruptedException e) {
+                    // if thrown by take(); ignore and try again.
+                    // if thrown by get(), fetch is still in progress. ignore.
+                } catch (ExecutionException e) {
+                    Log.e(TAG, "Prefetch action threw an exception");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/edu/umich/eac/FetchFuture.java
+++ src/edu/umich/eac/FetchFuture.java
@@ -1,55 +1,94 @@
 package edu.umich.eac;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
-private class FetchFuture<V> implements Future<V> {
-    Future<V> fetchFuture;
+class FetchFuture<V> implements Future<V> {
+    Future<V> realFuture;
     Callable<V> fetcher;
+    boolean cancelled;
     V result;
     private EnergyAdaptiveCache cache;
     
     FetchFuture(Callable<V> fetcher_, EnergyAdaptiveCache cache_) {
-        fetchFuture = null;
+        realFuture = null;
         fetcher = fetcher_;
+        cancelled = false;
         result = null;
         cache = cache_;
     }
     
     public boolean cancel(boolean mayInterruptIfRunning) {
+        // XXX: needs a closer look and testing.
+        if (cancelled) {
+            return true;
+        }
         
-    }
-    
-    private synchronized void establishFuture() {
-        if (fetchFuture == null) {
-            // haven't submitted it yet; better do it now
-            fetchFuture = cache.submit(fetcher);
+        Future<V> f = getFutureRef();
+        if (f != null) {
+            cancelled = f.cancel(mayInterruptIfRunning);
+            return cancelled;
+        } else {
+            cache.remove(this);
+            cancelled = true;
+            return true;
         }
     }
     
-    public V get() {
+    private synchronized void establishFuture() throws CancellationException {
+        if (cancelled) throw new CancellationException();
+        if (realFuture == null) {
+            // haven't submitted it yet; better do it now
+            realFuture = cache.submit(this);
+        }
+    }
+    
+    private synchronized Future<V> getFutureRef() {
+        return realFuture;
+    }
+    
+    public V get() throws InterruptedException, ExecutionException, 
+                          CancellationException {
+
         establishFuture();
-        result = fetchFuture.get();
+        result = realFuture.get();
         cache.remove(this);
         return result;
     }
     
-    public V get(long timeout, TimeUnit unit) {
+    public V get(long timeout, TimeUnit unit) 
+        throws InterruptedException, ExecutionException, 
+               TimeoutException, CancellationException {
+            
         establishFuture();
-        result = fetchFuture.get(timeout, unit);
+        result = realFuture.get(timeout, unit);
         cache.remove(this);
         return result;
     }
     
     public boolean isCancelled() {
-        
+        Future <V> f = getFutureRef();
+        return cancelled || (f != null && f.isCancelled());
     }
     
     public boolean isDone() {
-        Future<V> f;
-        synchronized (this) {
-            f = fetchFuture;
-        }
+        Future<V> f = getFutureRef();
         return (f != null && f.isDone());
+    }
+    
+    void startAsync() throws ExecutionException {
+        try {
+            get(0, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            // thrown by get(); expected
+            // nothing; I just wanted to get this fetch a-rollin'
+        } catch (InterruptedException e) {
+            // if thrown by get(), fetch is still in progress,
+            //  even though get() was somehow interrupted. ignore.
+        }
     }
 }
